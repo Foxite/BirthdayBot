5291e112e74064097f8417c8034fc17e6db45867
Dirk Kok <the@dirkkok.nl>
2022-07-19 17:28:31 +0200

    initial commit

---
+++ BirthdayBot/Birthday.cs
@@ -0,0 +1,10 @@
+using DSharpPlus.Entities;
+
+namespace BirthdayBot; 
+
+public record Birthday(DateTimeOffset Date, string Name, ulong Id) {
+	public DiscordWebhookBuilder Present() {
+		return new DiscordWebhookBuilder()
+			.WithContent($"🎉 <@{Id}> Gefeliciteerd met je verjaardag {Name}! 🥳");
+	}
+}
---
+++ BirthdayBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using BirthdayBot;
+using CsvHelper;
+using CsvHelper.Configuration;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+using var csvReader = new CsvReader(File.OpenText(Environment.GetEnvironmentVariable("CSV_PATH") ?? "birthdays.csv"), new CsvConfiguration(CultureInfo.InvariantCulture));
+csvReader.Read();
+csvReader.ReadHeader();
+
+List<Birthday> birthdays = csvReader.GetRecords<Birthday>()
+	.Where(birthday => birthday.Date.Date == DateTimeOffset.Now.Date).ToList();
+
+if (birthdays.Count > 0) {
+	var discord = new DiscordWebhookClient();
+	DiscordWebhook webhook = await discord.AddWebhookAsync(new Uri(Environment.GetEnvironmentVariable("WEBHOOK_URI")!));
+
+	foreach (Birthday birthday in birthdays) {
+		await webhook.ExecuteAsync(birthday.Present());
+	}
+}
